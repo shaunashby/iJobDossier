46292256d359dc6eda5b5d300d31cfe8d7b5d707
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-08 16:20:56 +0100

    Added initial app files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IJobDossier::Application.config.secret_token = 'd002a888b8284f70ac146b013377564f646ccb63b51b365c9eb3cddf643694512318657b1939a95102c805522ea9aee9c48a87fda357e5872ae52a9304b9121c'
