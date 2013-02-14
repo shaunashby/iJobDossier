eb482f72d03a6da681386732d4427ac1654883ac
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-14 18:14:17 +0100

    Remove explicit ID from table name in migration

--- db/migrate/20130208153210_create_job_application_pools.rb
+++ db/migrate/20130208153210_create_job_application_pools.rb
@@ -1,7 +1,6 @@
 class CreateJobApplicationPools < ActiveRecord::Migration
   def change
     create_table :job_application_pools do |t|
-      t.integer :id
       t.string :user
 
       t.timestamps
