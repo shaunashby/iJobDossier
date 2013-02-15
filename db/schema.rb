32b980c8844e57884f41ab92ff1396775c2174b6
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-15 17:43:56 +0100

    Update to new schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208231132) do
+ActiveRecord::Schema.define(:version => 20130215163606) do
 
   create_table "application_targets", :force => true do |t|
     t.integer  "job_application_id"
