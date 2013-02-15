bcdacbf35229759cde076c375c16f8d911e1e72b
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-15 18:24:39 +0100

    Validator is called Uniqueness, not Unique.

--- app/models/job_application_pool.rb
+++ app/models/job_application_pool.rb
@@ -1,7 +1,17 @@
+# == Schema Information
+#
+# Table name: job_application_pools
+#
+#  id         :integer          not null, primary key
+#  user       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class JobApplicationPool < ActiveRecord::Base
   attr_accessible :user
 
   has_many :job_applications
 
-  validates :user, presence: true, unique: true
+  validates :user, presence: true, uniqueness: true
 end
