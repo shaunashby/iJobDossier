3d0964e2ae6d3989e72bf7c6b641acaae4ea0d54
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-15 18:17:15 +0100

    Use ruby 1.9.x format for options hash in validates method calls.

--- app/models/application_target.rb
+++ app/models/application_target.rb
@@ -1,7 +1,7 @@
 class ApplicationTarget < ActiveRecord::Base
   belongs_to :job_application
 
-  validates :company, :presence => true
-  validates :contact, :presence => true
-  validates :address, :presence => true
+  validates :company, presence: true
+  validates :contact, presence: true
+  validates :address, presence: true
 end
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -2,7 +2,7 @@ class JobApplication < ActiveRecord::Base
   has_one :application_target
   belongs_to :job_application_pool
 
-  validates :jobtitle, :presence => true
-  validates :jobref,   :presence => true
-  validates :deadline, :presence => true
+  validates :jobtitle, presence: true
+  validates :jobref,   presence: true
+  validates :deadline, presence: true
 end
