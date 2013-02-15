08b7a05e4df218a06d797d711053d898c54199d0
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-15 17:40:53 +0100

    Redo migration for JobApplicationPool

--- db/migrate/20130208153210_create_job_application_pools.rb
+++
@@ -1,9 +0,0 @@
-class CreateJobApplicationPools < ActiveRecord::Migration
-  def change
-    create_table :job_application_pools do |t|
-      t.string :user
-
-      t.timestamps
-    end
-  end
-end
