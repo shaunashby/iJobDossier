3f063a34ca508a237f7dc68824c93fae0b99ce61
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-02-25 14:51:19 +0100

    Change notes column type to text in ApplicationTarget and Dossier models.

---
+++ db/migrate/20130225134924_change_notes_type_to_text_in_dossier.rb
@@ -0,0 +1,9 @@
+class ChangeNotesTypeToTextInDossier < ActiveRecord::Migration
+  def up
+    change_column :application_targets, :notes, :text
+  end
+
+  def down
+    change_column :application_targets, :notes, :string
+  end
+end
