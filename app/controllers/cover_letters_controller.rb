7fc5e0df8dcae2acf0532b0a076c2db571a6716b
Shaun Ashby <shaun.ashby@gmail.com>
2019-04-29 11:33:19 +0200

    Render cover letters for applications

--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -1,8 +1,13 @@
 class CoverLettersController < ApplicationController
-
+  layout "main"
   # GET /dossiers/:dossier_id/cover_letter/:id(.:format) dossier_cletter
   def show
-    render :nothing
+    @cover_letter = CoverLetter.new({ lang: "EN" })
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cover_letter }
+    end
   end
 
   # GET /dossiers/:dossier_id/cover_letter/new(.:format) new_dossier_cletter 
@@ -15,6 +20,11 @@ class CoverLettersController < ApplicationController
 
   # POST /dossiers/:dossier_id/cover_letter(.:format)
   def create
+    logger.debug("POST to create called")
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # PUT /dossiers/:dossier_id/cover_letter/:id(.:format)
